67e216f15cbdfc8cd15953bade94fcaa3aad0f6c
sidibea <sidibesekouassane@gmail.com>
2017-02-22 15:34:38 +0000

    Creation de l'entite

---
+++ src/NB/UsersBundle/Entity/Users.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace NB\UsersBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Users
+ *
+ * @ORM\Table(name="users")
+ * @ORM\Entity(repositoryClass="NB\UsersBundle\Repository\UsersRepository")
+ */
+class Users
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
