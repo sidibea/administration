4e98d926724cdc235dbd852bea3e4369c47b4a70
sidibea <sidibesekouassane@gmail.com>
2017-02-22 15:24:12 +0000

    Generation

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-
+            new FOS\UserBundle\FOSUserBundle(),
+            new NB\MainBundle\NBMainBundle(),
+            new NB\UsersBundle\NBUsersBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
